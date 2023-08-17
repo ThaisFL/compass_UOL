97376393389db6b0af1b0896e18a97d28bdd19a3
ThaisFL <thais.flores14@gmail.com>
2023-08-16 21:15:14 -0300

    arquivos de desafios do curso python

---
+++ Sprint_3/desafios.py
@@ -0,0 +1,12 @@
+# DESAFIO 1
+"""
+Calcule o percentual das despesas em relaçao ao salario:
+salario = 3456.45
+despesas = 2456.2
+"""
+salario = 3450.45 
+despesas = 2456.2 
+
+percentual = despesas / salario * 100
+
+
